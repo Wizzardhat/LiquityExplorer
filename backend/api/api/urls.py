7793752df8e236fa52e789cb43308c6dd7959d96
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-09-02 23:27:20 +0200

    ✨ Add staked lqty amount endpoint

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_trove_stake, get_historical_number_of_troves
+from .views import get_trove_stake, get_historical_number_of_troves, get_staked_lqty_token_amount
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trove-stake/', get_trove_stake, name='trove-stake'),
     path('get_historical_number_of_troves/',
          get_historical_number_of_troves, name='historical-number-of-troves')
+    path('get_staked_lqty_token_amount/',
+         get_staked_lqty_token_amount, name='staked-lqty-token-amount')
 ]
