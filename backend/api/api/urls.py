8ef94c1cfc44dc5e6e2d4f68c29f0c2dda31137d
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-09-02 23:13:58 +0200

    🎨 Format code

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -5,5 +5,6 @@ from .views import get_trove_stake, get_historical_number_of_troves
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trove-stake/', get_trove_stake, name='trove-stake'),
-    path('get_historical_number_of_troves/', get_historical_number_of_troves, name='historical-number-of-troves')
+    path('get_historical_number_of_troves/',
+         get_historical_number_of_troves, name='historical-number-of-troves')
 ]
