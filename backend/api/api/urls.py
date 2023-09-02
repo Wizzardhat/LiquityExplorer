154cf986ed40e41f1b669098a8399209d6adcf43
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-09-02 23:28:49 +0200

    🐛 Add missing comma

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('trove-stake/', get_trove_stake, name='trove-stake'),
     path('get_historical_number_of_troves/',
-         get_historical_number_of_troves, name='historical-number-of-troves')
+         get_historical_number_of_troves, name='historical-number-of-troves'),
     path('get_staked_lqty_token_amount/',
          get_staked_lqty_token_amount, name='staked-lqty-token-amount')
 ]
