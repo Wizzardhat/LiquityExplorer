a26c5f968fedb135de159763caef5e3e05e8d980
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-09-02 19:44:26 +0200

    👷 Add line chart

---
+++ frontend/src/components/line-chart.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from "svelte";
+    import { Chart } from "chart.js/auto";
+
+    onMount(() => {
+        const ctx = document.getElementById("myChart");
+
+        new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [
+                    {
+                        label: "# of Votes",
+                        data: [12, 19, 3, 5, 2, 3],
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        });
+    });
+</script>
+
+<div>
+    <canvas id="myChart" />
+</div>
