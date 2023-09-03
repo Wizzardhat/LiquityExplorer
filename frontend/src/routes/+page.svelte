6b12c8d3f093d371a664c60ed49063dae30b4b00
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-09-03 05:14:50 +0200

    ✨ Add app name and tables

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,6 +4,8 @@
     import TroveChart from "../components/historical-troves.svelte";
 </script>
 
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Liquity Protocol Explorer</h1>
+<br/>
 <TroveChart />
 <BestWorstTroves />
 <EthLusd />
