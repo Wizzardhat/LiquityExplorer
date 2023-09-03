03740cb00d8561c5ec24cebf43736a0561d306e8
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-09-03 03:45:01 +0200

    ✨ Add loaders to best/worst troves

--- frontend/src/components/best-worst-troves.svelte
+++ frontend/src/components/best-worst-troves.svelte
@@ -1,50 +1,64 @@
 <script>
     import { onMount } from "svelte";
-    import { Chart } from "chart.js/auto";
+    import Loading from "./loading.svelte";
+
     let bestTroves = [];
     let worstTroves = [];
+
+    let isLoading = true; // Used for loading animation
+
     async function fetchBestWorstTroves(url) {
-        const response = await fetch(
-            'http://127.0.0.1:8000/'+url,
-            {
-                credentials: 'include',
-            },
-            ); // Replace with your API endpoint URL
+        const response = await fetch("http://127.0.0.1:8000/" + url, {
+            credentials: "include",
+        }); // Replace with your API endpoint URL
         if (!response.ok) {
-            throw new Error('Failed to fetch data from the API');
+            throw new Error("Failed to fetch data from the API");
         }
         const data = await response.json();
+        isLoading = false;
         return data.troves;
     }
 
     onMount(async () => {
-        bestTroves = await fetchBestWorstTroves('get-best-troves/');
-        worstTroves = await fetchBestWorstTroves('get-worst-troves/');
+        bestTroves = await fetchBestWorstTroves("get-best-troves/");
+        worstTroves = await fetchBestWorstTroves("get-worst-troves/");
     });
 </script>
 
-<h3>Best Troves</h3>
-<div >
-    {#each bestTroves as bestTrove (bestTrove.array_index)}
-    <div>
-        <span>Address: {bestTrove.address}</span>
-        <span>Collateral: {bestTrove.collateral}</span>
-        <span>Debt: {bestTrove.debt}</span>
-        <span>Stake: {bestTrove.stake}</span>
-        <br>
-    </div>
-  {/each}
-</div>
+<main>
+    {#if isLoading}
+        <Loading />
+    {:else}
+        <h3>Best Troves</h3>
+        <div>
+            {#each bestTroves as bestTrove (bestTrove.array_index)}
+                <div>
+                    <span>Address: {bestTrove.address}</span>
+                    <span>Collateral: {bestTrove.collateral}</span>
+                    <span>Debt: {bestTrove.debt}</span>
+                    <span>Stake: {bestTrove.stake}</span>
+                    <br />
+                </div>
+            {/each}
+        </div>
+    {/if}
+</main>
 
-<h3>Worst Troves</h3>
-<div >
-    {#each worstTroves as worstTrove (worstTrove.array_index)}
-    <div>
-        <span>Address: {worstTrove.address}</span>
-        <span>Collateral: {worstTrove.collateral}</span>
-        <span>Debt: {worstTrove.debt}</span>
-        <span>Stake: {worstTrove.stake}</span>
-        <br>
-    </div>
-  {/each}
-</div>
+<main>
+    {#if isLoading}
+        <Loading />
+    {:else}
+        <h3>Worst Troves</h3>
+        <div>
+            {#each worstTroves as worstTrove (worstTrove.array_index)}
+                <div>
+                    <span>Address: {worstTrove.address}</span>
+                    <span>Collateral: {worstTrove.collateral}</span>
+                    <span>Debt: {worstTrove.debt}</span>
+                    <span>Stake: {worstTrove.stake}</span>
+                    <br />
+                </div>
+            {/each}
+        </div>
+    {/if}
+</main>
